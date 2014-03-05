3b388ac210b5dec590e0028cf9c64f263f5069ac
Apure Gupta <apurve@gotechy.in>
2014-03-06 02:10:13 +0530

    Signed-off-by: Apure Gupta <apurve@gotechy.in>

---
+++ src/com/thekha/vendor/activity/ProfileActivity.java
@@ -0,0 +1,23 @@
+package com.thekha.vendor.activity;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class ProfileActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_profile);
+		setTitle(R.string.profile_name);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return true;
+	}
+
+}
