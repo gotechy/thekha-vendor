39136a86883aa3d0a5fcb35046074a655cc7d3dd
Apure Gupta <apurve@gotechy.in>
2014-03-07 02:52:48 +0530

    Linked to edit activity, edit activity not complete.
    Signed-off-by: Apure Gupta <apurve@gotechy.in>

--- src/com/thekha/vendor/activity/EditBusinessActivity.java
+++ src/com/thekha/vendor/activity/EditBusinessActivity.java
@@ -1,25 +1,24 @@
 package com.thekha.vendor.activity;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class BusinessActivity extends Activity {
-	// TODO - add website to ui
+public class EditBusinessActivity extends Activity {
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_business);
-		setTitle(R.string.business);
+		setContentView(R.layout.activity_edit_business);
+		setTitle(R.string.edit_business);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.business, menu);
+		getMenuInflater().inflate(R.menu.edit_business, menu);
 		return super.onCreateOptionsMenu(menu);
 	}
 	
@@ -27,10 +26,8 @@ public class BusinessActivity extends Activity {
 	  public boolean onOptionsItemSelected(MenuItem item) {
 	    switch (item.getItemId()) {
 	    // action with ID action_refresh was selected
-	    case R.id.business_menu:
+	    case R.id.edit_business_menu:
 	      Toast.makeText(this, "Edit Selected", Toast.LENGTH_SHORT).show();
-	      Intent i = new Intent(getApplicationContext(), EditBusinessActivity.class);
-	      startActivity(i);
 	      break;
 	    default:
 	      break;
