bf3af6d1247d31a8e70747c13446653d1d8b4686
Apure Gupta <apurve@gotechy.in>
2014-03-07 00:16:21 +0530

    Signed-off-by: Apure Gupta <apurve@gotechy.in>

--- src/com/thekha/vendor/activity/ProfileActivity.java
+++ src/com/thekha/vendor/activity/ProfileActivity.java
@@ -3,6 +3,8 @@ package com.thekha.vendor.activity;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ProfileActivity extends Activity {
 
@@ -16,8 +18,21 @@ public class ProfileActivity extends Activity {
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		//getMenuInflater().inflate(R.menu.profile, menu);
-		return false;
+		getMenuInflater().inflate(R.menu.profile, menu);
+		return super.onCreateOptionsMenu(menu);
 	}
+	
+	@Override
+	  public boolean onOptionsItemSelected(MenuItem item) {
+	    switch (item.getItemId()) {
+	    // action with ID action_refresh was selected
+	    case R.id.edit_profile:
+	      Toast.makeText(this, "Edit Selected", Toast.LENGTH_SHORT).show();
+	      break;
+	    default:
+	      break;
+	    }
+	    return true;
+	  } 
 
 }
