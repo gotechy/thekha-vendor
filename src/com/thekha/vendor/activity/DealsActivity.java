09f907bf886ef4f60c4fdc0299787c3cd31b2ecc
Gaurav Rana <gaurav.rana@gotechy.in>
2014-03-07 13:30:14 +0530

    Edit Deal Activity
    Signed-off-by: Gaurav Rana  <gaurav.rana@gotechy.in>

---
+++ src/com/thekha/vendor/activity/DealsActivity.java
@@ -0,0 +1,27 @@
+package com.thekha.vendor.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class DealsActivity extends Activity {
+	
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_add_edit_deals);
+		setTitle(R.string.title_deals);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.business, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
+	
+
+}
